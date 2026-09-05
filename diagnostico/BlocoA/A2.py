345e4836d86b7486ce5a3f07812d3d6cf742a680
Fernando Fiore <fernandofiore310@gmail.com>
2026-09-04 18:05:49 -0700

    Exercicio A2 feito e arrumado (acho)

--- diagnostico/BlocoA/A2.py
+++ diagnostico/BlocoA/A2.py
@@ -1,34 +1,34 @@
 def rank_alunos(dic):
-    rank = []
-    for nome, lista_notas in dic.items():
+    lista_alunos = []
+
+    for aluno, lista_notas in dic.items():
         soma = 0
-        size = 0
-        # print(lista_notas)
         for nota in lista_notas:
             soma += nota
-            size += 1
-        media = soma/size
-        tupla = (nome, media)
-        rank.append(tupla)
+        if len(lista_notas) != 0:
+            media = round(soma/len(lista_notas), 2)
+        else:
+            media = 0.00
+        tupla = (aluno, media)
+        lista_alunos.append(tupla)
 
-    c = 0
-    top = []
-    while c < 3:
-        maior = 0
-        nome_aluno = ""
-        for t in rank:
-            if t[1] > maior:
-                maior = t[1]
-                nome_aluno = t[0]
-        tupla2 = (nome_aluno, maior)
-        top.append(tupla2)
+    # print(lista_alunos)
+    lista_alunos = sorted(lista_alunos, key=lambda item: (-item[1], item[0]))
+    print(lista_alunos)
 
-    return top
+    for aluno in lista_alunos:
+        if aluno[1] == 0.0:
+            lista_alunos.remove(aluno)
+    print(lista_alunos)
+
+    return [lista_alunos[0], lista_alunos[1], lista_alunos[2]]
 
 dicionario = {"Marco" : [3.0, 6.50, 7.85],
               "Dani" : [9.0, 8.5, 10.0],
               "John" : [3.5, 4.75, 5.15],
-              "Pablo" : [6.0, 6.0, 6.0]
+              "Pablo" : [6.0, 6.0, 6.0],
+              "Gordon": [6.0, 6.0, 6.0],
+              "Jim": []
               }
 
 print(rank_alunos(dicionario))
