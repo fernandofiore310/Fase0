2353277c13c8ad0181c96408b37c27c9fec8a2a5
Fernando Fiore <fernandofiore310@gmail.com>
2026-09-04 18:06:22 -0700

    Exercicio A4 tambem resolvido

--- diagnostico/BlocoA/A4.py
+++ diagnostico/BlocoA/A4.py
@@ -1,16 +1,35 @@
 def conta_frequencia(texto, n, stopwords):
+    dicionario_palavras = {}
+    palavras_frequentes = []
+
     texto = texto.lower()
-    pontuacao = [".", ",", "?", "!"]
-    lista_palavras = []
+    lista_texto = texto.split()
+    for palavra in lista_texto:
+        if palavra not in dicionario_palavras:
+            dicionario_palavras[palavra] = 1
+        else:
+            dicionario_palavras[palavra] += 1
+    # print(dicionario_palavras)
+    print(sorted(dicionario_palavras.items()))
+    lista_freq = sorted(dicionario_palavras.items())
 
-    for p in texto:
-        palavra = ""
-        for c in p:
-            if c not in pontuacao:
-                palavra += c
-        if palavra not in stopwords:
-            lista_palavras.append(palavra)
+    i = 0
+    while i < n:
+        maior = 0
+        p = ""
+        index = 0
+        c = 0
+        for tupla in lista_freq:
+            if tupla[1] > maior:
+                maior = tupla[1]
+                p = tupla[0]
+                index = c
+            c += 1
+        palavras_frequentes.append(p)
+        del lista_freq[index]
+        i+=1
 
+    print(palavras_frequentes)
     
-
-print(conta_frequencia("Oi, tudo bem?", 3))
+texto = "Meu dia começou assim: mandei quatro ovos com manteiga, com duas fatias de bacon, um quarto de abacate, com um café preto para acompanhar e um suco de laranja natural."
+print(conta_frequencia(texto, 4, []))
