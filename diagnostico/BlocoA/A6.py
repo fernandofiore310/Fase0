a27f7c8013045f44804e9e4b3bbe6771bf173cb9
Fernando Fiore <fernandofiore310@gmail.com>
2026-09-07 20:07:50 -0700

    Fiz o exercicio em 11min e 15seg, e depois, passei cerca de 12 minutos tirando duvidas com o Gemini e preenchendo o arquivo com infos

--- diagnostico/BlocoA/A6.py
+++ diagnostico/BlocoA/A6.py
@@ -14,31 +14,9 @@ def conta_error(log):
                 dic[hora] += 1
     return dic
 
-
-log = """2026-09-03 08:12:07 ERROR Aplicação iniciada com sucesso
-2026-09-03 08:14:31 INFO Conexão com o banco de dados estabelecida
-2026-09-03 08:27:45 WARNING Tempo de resposta acima do esperado
-2026-09-03 09:03:18 INFO Usuário autenticado com sucesso
-2026-09-03 09:41:52 ERROR Falha ao carregar arquivo de configuração
-2026-09-03 09:42:10 INFO Tentando carregar configuração padrão
-2026-09-03 10:15:01 INFO Configuração padrão carregada com sucesso
-2026-09-03 10:48:33 WARNING Uso de memória acima de 75 por cento
-2026-09-03 11:22:10 INFO Requisição recebida no endpoint de usuários
-2026-09-03 11:22:11 ERROR Erro ao processar dados da requisição
-2026-09-03 11:57:19 INFO Requisição encerrada com status 500
-2026-09-03 12:34:04 INFO Nova tentativa de conexão com serviço externo
-2026-09-03 12:48:09 WARNING Serviço externo respondeu com atraso
-2026-09-03 13:05:12 INFO Resposta do serviço externo recebida
-2026-09-03 13:46:45 INFO Cache atualizado com sucesso
-2026-09-03 14:30:02 ERROR Não foi possível salvar registro no banco de dados
-2026-09-03 14:44:18 WARNING Tentativa de escrita será repetida
-2026-09-03 15:02:25 INFO Registro salvo com sucesso na segunda tentativa
-2026-09-03 15:39:37 INFO Processo de sincronização iniciado
-2026-09-03 16:18:06 WARNING Foram encontrados registros duplicados
-2026-09-03 16:52:44 INFO Registros duplicados foram ignorados
-2026-09-03 17:33:21 ERROR Falha de conexão com o servidor de autenticação
-2026-09-03 18:04:40 INFO Reconectando ao servidor de autenticação
-2026-09-03 18:17:46 INFO Conexão restabelecida com sucesso
-2026-09-03 19:26:10 INFO Aplicação operando normalmente"""
-
-print(conta_error(log))
+with open("logsim.txt", mode='r', encoding="utf-8") as file:
+    log = file.read() # Le todo o conteudo do arquivo e o retorna uma string no modo texto 'r' ou como uma sequencia de bytes 'rb'
+    # log = file.readlines() Lê todas as linhas e retorna uma lista de strings.
+    # log = file.readline() Lê uma única linha por vez, avançando o ponteiro do arquivo.
+    # for linha in file: sempre que precisa processar um arquivo texto sequencialmente garantindo eficiência de memória, especialmente quando o arquivo é grande demais para caber confortavelmente na RAM ou quando seu tamanho é desconhecido.
+    print(conta_error(log))
