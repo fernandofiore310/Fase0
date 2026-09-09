4eaaa5d7bde647dfae7bdbcd555d99f853702f3d
Fernando Fiore <fernandofiore310@gmail.com>
2026-09-08 18:23:05 -0700

    Arrumando correcao do ex18

--- drills/2026-09-08/Ex18.py
+++ drills/2026-09-08/Ex18.py
@@ -5,7 +5,8 @@ def conta_votos(votos):
     contagem = dict(Counter(votos))
     # print(contagem)
     candidato, votos = max(contagem, key=lambda x : contagem.values())
-    # nao seu tempo de acabar, mas o correto seria max(contagem, key=lambda x : contagem[x])
+    # nao seu tempo de acabar, mas o correto seria candidato = max(contagem, key=lambda x : contagem[x])
+    # candidato = max(contagem, key=lambda x : contagem[x])
 
     print(f"Mais votado: {candidato}")
 
